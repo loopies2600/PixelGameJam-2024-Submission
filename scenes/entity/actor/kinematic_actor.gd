8fa0e60cc0e1c2b28b812fb3ba5c625c4df1cce8
Leandro Sotelo <frutiloopies2600@gmail.com>
2024-05-12 18:08:04 -0300

    Control con flechitas

--- scenes/entity/actor/kinematic_actor.gd
+++ scenes/entity/actor/kinematic_actor.gd
@@ -3,9 +3,12 @@ class_name KinematicActor
 
 export (float) var base_speed := 128.0
 export (float, 0.0, 0.99) var steering := 0.0
+export (int) var max_health := 10
 
 var direction : Vector2 = Vector2.ZERO
 var velocity : Vector2 = Vector2.ZERO
+
+var health : int = max_health
 
 func get_walk_velocity() -> Vector2:
 	var new_velocity : Vector2 = direction * base_speed
