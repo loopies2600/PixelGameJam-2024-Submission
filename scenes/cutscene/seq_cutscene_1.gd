9617af93ea0162b4475cbd4fa0db0707afb5255a
Andrew White <91177473+andrewtypebeat@users.noreply.github.com>
2024-05-16 02:10:43 -0600

    pusheo

--- scenes/cutscene/seq_cutscene_1.gd
+++ scenes/cutscene/seq_cutscene_1.gd
@@ -1,5 +1,17 @@
 extends Cutscene
 
 func sequence():
-	chatter("test")
+	chatter("Sir Quejonte, you have failed me for the last time!")
+	chatter("In consequence of your treacherous actions, I hereby sentence you to absolute death!")
+	chatter("But my king! I swear the food bag was sitting in my room!")
+	chatter("Silence! The bag had my name stylishly written all over. Your actions are unredeemable.")
+	chatter("I, Burg R. King, shall put an end to you.")
+	chatter("But, since you were normally one of my most loyal men, you may have one last wish.")
+	chatter("Can I go swim?")
+	chatter("yeah sure.")
+	chatter("you have thirty seconds from now.")
 	
+	chatter("And so, Sir Quejonte went ahead and walked deep into the sea.")
+	chatter("With his armor still on for some reason, he sinked and sinked until he reached the bottom of the sea.")
+	
+	chatter("Right, I can breathe underwater.")
