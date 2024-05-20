1c2ef35a510f5143d4a88f05983b40029db282e8
Andrew White <91177473+andrewtypebeat@users.noreply.github.com>
2024-05-19 19:40:48 -0600

    FINAL

--- scenes/cutscene/seq_cutscene_4.gd
+++ scenes/cutscene/seq_cutscene_4.gd
@@ -7,26 +7,9 @@ const PRT_KING := preload("res://assets/sprites/portraits/port_sir_burgking.png"
 func sequence():
 	sleep(2)
 	
-	setNameplate("Poseidon")
-	setPortrait(PRT_POSEID)
-	chatter("I see your fast-food king sent you after my people in a mission to satiate his gargantuan hunger.")
-	chatter("It's just normal that you're also after me. Since I taste just like fish.")
-	
-		
-	playSound("res://assets/streams/sounds/ringtone.wav")
-	
-	sleep(0.3)
-	
-	playSound("res://assets/streams/sounds/beep.wav")
-	
-	setPortrait(PRT_KING)
-	chatter("We're not here to eat you! We're here to discuss that free-trade agreement we postponed last month. ")
-
-	setNameplate("Poseidon")
-	setPortrait(PRT_POSEID)
-	chatter("Ah, so this is merely a political strategy.")
-	chatter("I don't want your filthy hamburgers down here.")
-	
-	setPortrait(PRT_KING)
-	chatter("We shall see about that.")
-	chatter("Sir Quejonte! Kill him! Kill him so I can kill you later!")
+	setNameplate("")
+	setPortrait(null)
+	chatter("And so, The Great King feasted on every single one of the residents of the now lost kingdom of Atlantis. ")
+	chatter("Sir Quejonte was surprisingly spared by the o great and humble King of... land, in... some place.")
+	chatter("With Poseidon beat-up, there just was no way fish could exist anymore, so they went extinct. A very unfortunate ending for such a short tale.")
+	chatter("The End")
